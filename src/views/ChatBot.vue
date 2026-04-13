6e97b2bb9f38e82c03ac4da42e1be47aa50e25f2
luke <https://gitlab.com/luke.lin2com>
2026-04-13 15:33:24 +0800

    Revert "調整對話框"
    This reverts commit c8ff79ff3d5f08d894ee9a1e320cc025846c78cd.

--- src/views/ChatBot.vue
+++ src/views/ChatBot.vue
@@ -16,7 +16,7 @@
         :class="['message', msg.sender === 'user' ? 'user' : 'ai']"
       >
         <div class="emoji-avatar">
-          {{ msg.sender === 'user' ? '🧑' : '🤖' }}
+          {{ msg.sender === 'user' ? '🧑‍💻' : '🤖' }}
         </div>
 
         <div class="bubble">
